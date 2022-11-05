dbc7a350fb03f84eab6964ff7a45a47226690973
stemtazoo <tazoo210.504.ai@gmail.com>
2022-11-05 16:06:17 +0900

    revision

--- examples/reliability/streamlit_app_create_graph_reliability.py
+++ examples/reliability/streamlit_app_create_graph_reliability.py
@@ -4,6 +4,33 @@ import pandas as pd
 import plotly.express as px
 from lifelines import KaplanMeierFitter,NelsonAalenFitter
 
+#panda dataframe columns 01 to bool
+def columns_tobool(df): 
+    length=len(df)
+    for column in df.columns:
+        if len(df[(df[column]==0)|(df[column]==1)])==length:
+            df[column] = df[column].astype(bool) 
+    return df 
+
+#Reliability estimate
+def reliability_estimate(df,duration,observed):
+    T = df[duration]
+    E = df[observed]
+    #culc kmf
+    kmf = KaplanMeierFitter()
+    kmf.fit(T, event_observed=E)  # or, more succinctly, kmf.fit(T, E)
+    df_kmf=kmf.survival_function_
+    df_kmf['model']='KaplanMeierFitter'
+    df_kmf=df_kmf.rename(columns={'KM_estimate': 'reliability'})
+    #culc naf
+    naf = NelsonAalenFitter()
+    naf.fit(T, event_observed=E)
+    df_naf=naf.cumulative_hazard_
+    df_naf['reliability']=np.exp(-1*df_naf['NA_estimate'])
+    df_naf['model']='NelsonAalenFitter'
+    df_reliability=pd.concat([df_kmf,df_naf])
+    return df_reliability
+
 st.title('📉 Create a line chart of reliability')
 
 uploaded_file = st.file_uploader("Choose a csv file")
@@ -14,43 +41,56 @@ if uploaded_file is not None:
     except:
         df = pd.read_csv(uploaded_file, encoding='cp932')
 
+    #dataframe column 01 to bool
+    df=columns_tobool(df)
     #st.write('File name is ',uploaded_file.name)
     title_name=uploaded_file.name.replace('.csv','')
     with st.expander("See dataframe"):
         st.dataframe(df)
     #list
-    df2=df.select_dtypes(include=[int,float, bool])
+    df2=df.select_dtypes(include=[int,float])
     items=list(df2.columns)
     #Select array of durations
     duration = st.radio(
         "Select a array of durations",
         items)
     #Select observed array
-    items.remove(duration)
+    df2=df.select_dtypes(include=[bool])
+    items=list(df2.columns)
     observed = st.radio(
         "Select a either boolean or binary array representing whether the “death” was observed or not",
         items)
+    #Select group
+    items=list(df.columns)
+    items.remove(duration)
+    items.remove(observed)
+    items.insert(0,'None')
+    group = st.selectbox(
+    'If using Multiple group, please select columns.',
+    items)
     #lifelines
-    T = df[duration]
-    E = df[observed]
-    #culc kmf
-    kmf = KaplanMeierFitter()
-    kmf.fit(T, event_observed=E)  # or, more succinctly, kmf.fit(T, E)
-    #culc naf
-    naf = NelsonAalenFitter()
-    naf.fit(T, event_observed=E)
-    df=naf.cumulative_hazard_
-    df['NA_estimate']=np.exp(-1*df['NA_estimate'])
-    df=pd.merge(df,kmf.survival_function_,left_index=True,right_index=True)
-    #Create chart
-    title_name='Reliability Kaplan-Meier'
-    fig = px.line(df, x=df.index, y='KM_estimate', title=title_name)
+    if group != 'None':
+        names=df[group].unique().tolist()
+        df_reliability=pd.DataFrame()
+        for name in names:
+            df3=reliability_estimate(df[df[group]==name],duration,observed)
+            df3['name']=name
+            df_reliability=pd.concat([df_reliability,df3])
+        #Create chart
+        title_name='Reliability'
+        df_reliability=df_reliability[df_reliability['model']=='NelsonAalenFitter']
+        fig = px.line(df_reliability, x=df_reliability.index, y='reliability',color='name', title=title_name)
+    else:
+        df_reliability=reliability_estimate(df,duration,observed)
+        #Create chart
+        title_name='Reliability'
+        fig = px.line(df_reliability, x=df_reliability.index, y='reliability',color='model', title=title_name)
+       
     # Plot!
     st.plotly_chart(fig, use_container_width=True)
-    title_name='Reliability Nelson-Aalen'
-    fig = px.line(df, x=df.index, y='NA_estimate', title=title_name)
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True)
+    #show dataframe
+    with st.expander("See dataframe"):
+        st.dataframe(df_reliability)
 else:
         st.info(
             f"""
